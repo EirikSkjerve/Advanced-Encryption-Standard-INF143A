0a112d2cae91a613248fc61035e0b99d6cbecd75
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2023-02-13 15:31:40 +0100

    Gonna use own field multiplication implementation

---
+++ F2_8_implementation.py
@@ -0,0 +1,15 @@
+
+polynomial_1 = ["x^8", "x^4", "x^3", "x", "1"]
+polynomial_2 = ["x^7", "x^5", "x^4", "x^2", "1"]
+
+def add(polynomial_1, polynomial_2):
+    pass
+
+def multiply(polynomial_1, polynomial_2):
+    pass
+
+def converto_to_vector(polynomial):
+    pass
+
+def find_inverse(polynomial):
+    pass

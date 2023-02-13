fb7deba2d6f946d0934e6a299165e576aaf9ea53
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2023-02-13 12:37:06 +0100

    Import function

--- AES_encryption.py
+++ AES_encryption.py
@@ -1,12 +1,17 @@
-
+from utils import split_blocks, XOR
 
 
 
 
 def encrypt(plaintext, key)->str:
-        
+    '''First step is splitting plaintext into blocks of size 128'''
+    blocks = split_blocks(plaintext, 128)
+    
     return None
 
+def encryption_round(plain_block, round_key):
+    
+    return None
 
 
 

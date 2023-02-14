6cc900a86b2c986b7ca6bf53c6ff8ca984967a4e
Eirik Skjerve <tic013@uib.no>
2023-02-14 11:27:59 +0100

    Almost done, missing one hex conversion

--- AES_encryption.py
+++ AES_encryption.py
@@ -1,5 +1,6 @@
 from utils import split_blocks, XOR, s_box_get, binary_to_hex, hex_to_binary
 from generate_round_keys import generate_round_keys
+from F2_8_implementation import multiply, field_sum_list
 
 
 def encrypt(plaintext, key)->str:
@@ -28,18 +29,15 @@ def encryption_round(plain_block, round_key, round_number):
         block = (s_box_get(block))
         permuted_blocks.append(block)
 
-    main_block = shift_rows(permuted_blocks)
+    matrix = shift_rows(block_to_matrix(permuted_blocks))
     if round_number != 9:
-        '''
-        MixColumn
-        '''
-    main_block = [hex_to_binary(h) for h in sum(main_block, [])]
-    #flattens the matrix into a list
-    main_block = "".join(main_block)
+        matrix = mix_columns(matrix)
+    
+    main_block = matrix_to_block(matrix)
     main_block = XOR(main_block, round_key)
     return main_block
 
-def shift_rows(block):
+def block_to_matrix(block):
     eight_bit_blocks = block
     hex_blocks = [binary_to_hex(b) for b in eight_bit_blocks]
     matrix = [["","","",""],
@@ -51,12 +49,21 @@ def shift_rows(block):
         row = h%4
         column = h//4
         matrix[row][column] = hex_blocks[h]
+    return matrix
+
+def matrix_to_block(matrix):
+    #flattens the matrix into a list
+    #this method is apparently not efficient in terms of space time complexity
+    main_block = [hex_to_binary(h) for h in sum(matrix, [])]
+    main_block = "".join(main_block)
+    return main_block
+
+def shift_rows(matrix):
     
     matrix[1] = shift_left(matrix[1],1)
     matrix[2] = shift_left(matrix[2],2)
     matrix[3] = shift_left(matrix[3],3)
     
-
     return matrix
 
 def shift_left(row, num_shifts):
@@ -64,6 +71,25 @@ def shift_left(row, num_shifts):
     for r in range(len(row)):
         temp[(r-num_shifts)%4] = row[r]
     return temp
+
+def mix_columns(matrix):
+    #fixed matrix A
+    A = [[2,3,1,1],[1,2,3,1],[1,1,2,3],[3,1,1,2]]
+    #empty initialized product matrix
+    product_matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    for i in range(4):
+        for j in range(4):
+            A_row = A[i]
+            input_column = [int(matrix[x][j],16) for x in range(4)]
+            
+            product_matrix[i][j] = field_sum_list([multiply(A_row[x],input_column[x]) for x in range(4)])
+    
+    
+    return product_matrix
+
+def integer_matrix_to_hex(int_matrix):
+    
+    return None
 
 def decrypt(ciphertext, key)->str:
     return None
--- F2_8_implementation.py
+++ F2_8_implementation.py
@@ -1,20 +1,16 @@
 
 #base_polynomial = ["x^8", "x^4", "x^3", "x", "1"] #the AES polynomial
 
-irreducible_polynomial = int("100011011", 2)
-p = 253
-q = 113
-
+primal_poly = int("100011011", 2)
 field_characteristic = 2
 field_degree = 8
 
-
 #thanks to Nikolay S. Kaleyski for C code, and thanks to chatgpt for translation to python
-def multiply(p, q, primal_poly, degree):
+def multiply(p, q):
     result = 0
     # The value at which we should reduce using the irreducible polynomial
     cutoff = 1
-    for i in range(degree-1):
+    for i in range(field_degree-1):
         cutoff <<= 1
     while p and q:
         if q & 1: # If b is odd, add a to the total
@@ -26,9 +22,13 @@ def multiply(p, q, primal_poly, degree):
         q >>= 1
     return result
 
+def field_sum_list(list)->str:
+    
+    sum = 0
+    for i in list:
+        sum^=i
 
-def add(p, q):
-    return p^q
+    return sum
 
 
 '''
